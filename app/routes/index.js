54e230e3294608db9465e94568b51b9dec566148
Matt Dean <matt.dean.001@gmail.com>
2016-09-01 00:10:51 +0100

    leaflet map working plus added new route

--- app/routes/index.js
+++ app/routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Developer Onboarding' });
 });
 
+router.get('/map', function(req, res, next) {
+  res.render('map', { title: 'Developer Onboarding' });
+});
+
 module.exports = router;
